c2c949f1ec11c1112051085e84f62944ac5ed927
Abhinav <abhinav.k@xcitech.in>
2022-08-25 15:16:33 +0530

    analytics script added in all pages

---
+++ assets/js/analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-238218478-1');
+
+  gtag('config', 'G-VTZJ8W6BR8');
